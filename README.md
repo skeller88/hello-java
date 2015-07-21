4c696983791cc33d7e97bc51520005b577c2cc27
Shane Keller <skeller@fitbit.co>
2015-07-21 16:24:44 -0700

    Add guava dependency and passing test.

---
+++ README.md
@@ -0,0 +1,3 @@
+### Fitbit Versions
+Gradle - 2.2.1
+Tomcat - 7.0.56
--- src/test/java/CalculatorUnitTest.java
+++ src/test/java/CalculatorUnitTest.java
@@ -7,14 +7,24 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 
+import static org.hamcrest.CoreMatchers.is;
+
 public class CalculatorUnitTest {
     @Rule
     public ErrorCollector errorCollector = new ErrorCollector();
 
     @Test
-    public void evaluatesExpression() {
+    public void evaluatesInvalidExpressions() {
         Calculator calculator = new Calculator();
         int shouldBeSeven = calculator.returnSeven();
-        assertEquals(7, shouldBeSeven);
+        errorCollector.checkThat("incorrect number 6", 6, is(shouldBeSeven));
+        errorCollector.checkThat("incorrect number 8", 8, is(shouldBeSeven));
+    }
+
+    @Test
+    public void evaluatesValidExpressions() {
+        Calculator calculator = new Calculator();
+        int shouldBeSeven = calculator.returnSeven();
+        errorCollector.checkThat("correct number 7", 7, is(shouldBeSeven));
     }
 }
